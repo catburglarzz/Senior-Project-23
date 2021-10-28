415111185709b818d1c4b9dd28e7e92aa70ac71b
Eric Nguyen <eric.nguyen02@sjsu.edu>
2021-10-28 02:32:33 -0700

    Camera Boundary, Deathzones, respawn, slope "fix"
    The player camera now has a main boundary that it cannot leave, implemented with cinemacamera confinement extension
    
    "Death Zones" are implemented where they teleport the player back to the respawn point
    
    Slopes now do not push slide you back down, but this is temporary. Going up a slope now has a bounce (needs fix)

---
+++ Senior-Proj-23/Assets/Scripts/Player/Kill_Player.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kill_Player : MonoBehaviour
+{
+    [SerializeField] Transform spawnPoint;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.transform.CompareTag("Player"))
+        {
+            collision.transform.position = spawnPoint.position;
+        }
+    }
+}
