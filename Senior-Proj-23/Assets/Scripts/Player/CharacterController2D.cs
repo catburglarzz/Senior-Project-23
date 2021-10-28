f292b3189b21c5dde4b9b318110271d902b371cd
Johnny Nguyen <91231359+catburglarzz@users.noreply.github.com>
2021-10-27 19:00:08 -0700

    Update CharacterController2D.cs

--- Senior-Proj-23/Assets/Scripts/Player/CharacterController2D.cs
+++ Senior-Proj-23/Assets/Scripts/Player/CharacterController2D.cs
@@ -138,7 +138,7 @@ public class CharacterController2D : MonoBehaviour
 		{
 			// Add a vertical force to the player.
 			m_Grounded = false;
-		m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
+		m_Rigidbody2D.velocity = new Vector2(m_Rigidbody2D.velocity.x,m_JumpForce);
 	}
 	
 	m_Grounded = true;
