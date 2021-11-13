0c9f85245b933d386e20e4776f2fada4f1bb8bd3
catburglarzz <johnny.nguyen@sjsu.edu>
2021-11-13 12:41:12 -0800

    Player hitbox, animation, enemy HP, enemy death.
    MISSING: enemy death animation and enemy attacked animation

---
+++ Senior-Proj-23/Assets/playerCombat.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerCombat : MonoBehaviour
+{
+    public Animator animator;
+
+    public Transform attackPoint;
+    public float attackRange = 0.5f;
+    public float attackRate = 2f;
+    float nextAttackTime = 0f;
+    public int attackDamage = 40;
+
+    public LayerMask enemyLayers;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time >= nextAttackTime)
+        {
+            if(Input.GetKeyDown(KeyCode.Q)) //Whatever attack key is
+            {
+                Attack();
+                nextAttackTime = Time.time + 1f / attackRate;
+            }
+        }
+        
+    }
+    
+    void Attack()
+    {
+        //Play an attack animation
+        animator.SetTrigger("Attack");
+
+        //Detect enemies in range
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers); // creates circle to detect attack zone
+
+        //Damage them
+        foreach(Collider2D enemy in hitEnemies)
+        {
+            Debug.Log("We hit" + enemy.name);
+            enemy.GetComponent<enemy>().TakeDamage(attackDamage);
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if(attackPoint == null)
+        {
+            return;
+        }
+
+
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+}
