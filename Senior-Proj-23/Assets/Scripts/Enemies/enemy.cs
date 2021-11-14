68af595b4cdd6293e7d9d9be880923911e82b8f5
Walton Ma <walton.ma@SJSU.edu>
2021-11-13 21:08:19 -0800

    Moved scripts to "Scripts" folder, expanded enemy's "Dead" state

--- Senior-Proj-23/Assets/Scripts/Enemies/enemy.cs
+++ Senior-Proj-23/Assets/Scripts/Enemies/enemy.cs
@@ -28,5 +28,19 @@ public class enemy : MonoBehaviour
     void Die()
     {
         Debug.Log("Enemy died!");
+
+        // Disable the enemy
+        GetComponent<Collider2D>().enabled = false;
+        //foreach (Collider2D c in this.GetComponents<Collider2D>())
+        //{
+        //    c.enabled = false;
+        //    //Physics2D.IgnoreCollision(CharacterController2D.GetComponent<Collider2D>(), c);
+        //}
+
+        this.GetComponent<Animator>().enabled = false; // jerry rigged way to make enemy "appear" dead, due to no dedicated death animation
+        this.GetComponent<AIPatrol>().enabled = false;
+
+
+        this.enabled = false;
     }
 }
