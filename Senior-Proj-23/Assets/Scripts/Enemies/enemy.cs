f6923265e89353cf196c4832876366503fa1aa0e
Walton Ma <walton.ma@SJSU.edu>
2021-11-14 16:59:37 -0800

    Added enemy hit animation, enemy properly disappears/is destroyed when killed

--- Senior-Proj-23/Assets/Scripts/Enemies/enemy.cs
+++ Senior-Proj-23/Assets/Scripts/Enemies/enemy.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class enemy : MonoBehaviour
 {
+    public Animator animator;
+
     public int maxHealth = 100;
     int currentHealth;
     // Start is called before the first frame update
@@ -17,6 +19,7 @@ public class enemy : MonoBehaviour
         currentHealth -= damage;
 
         // Play hurt animation
+        animator.SetTrigger("Hurt");
 
         if(currentHealth <= 0)
         {
@@ -31,16 +34,8 @@ public class enemy : MonoBehaviour
 
         // Disable the enemy
         GetComponent<Collider2D>().enabled = false;
-        //foreach (Collider2D c in this.GetComponents<Collider2D>())
-        //{
-        //    c.enabled = false;
-        //    //Physics2D.IgnoreCollision(CharacterController2D.GetComponent<Collider2D>(), c);
-        //}
-
-        this.GetComponent<Animator>().enabled = false; // jerry rigged way to make enemy "appear" dead, due to no dedicated death animation
         this.GetComponent<AIPatrol>().enabled = false;
-
-
+        Destroy(gameObject, 1);
         this.enabled = false;
     }
 }
